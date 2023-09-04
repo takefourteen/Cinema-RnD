b8ef758bb336cb160057649d5125899fd3978909
Ismail <shaikhnagismail@gmail.com>
2023-09-04 21:01:08 +0200

    responsive font size

--- components/application-route/home/ColorFulBanner.tsx
+++ components/application-route/home/ColorFulBanner.tsx
@@ -2,16 +2,18 @@ import PopcornImg from "./PopcornImg";
 
 const ColorFulBanner = () => {
   return (
-    <div className="box-shadow flex  w-full items-center justify-center gap-x-4 bg-radial-gradient p-[22px] text-white">
-      <PopcornImg />
+    <div className="box-shadow flex  w-full items-center justify-center gap-x-4 bg-radial-gradient p-6 text-white">
+      <div className="mr-2">
+        <PopcornImg />
+      </div>
 
       <div className="flex flex-col gap-y-2">
-        <p className="text-2xl font-bold">
+        <p className="text-xl font-bold lg:text-2xl">
           Discover the Ultimate Streaming Experience
         </p>
 
-        <p className="text-base">Stream anytime, anywhere, on any device.</p>
-        <p className="text-base">
+        <p className="text-sm lg:text-base">Stream anytime, anywhere, on any device.</p>
+        <p className="text-sm lg:text-base">
           Start watching now with no subscription required.
         </p>
       </div>
