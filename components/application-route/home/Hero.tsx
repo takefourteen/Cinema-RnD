5c58afa33c4bdb025e0a180f201e8b8b19726543
Ismail <shaikhnagismail@gmail.com>
2023-09-04 20:52:34 +0200

    responsive font size

--- components/application-route/home/Hero.tsx
+++ components/application-route/home/Hero.tsx
@@ -7,17 +7,19 @@ type Props = {};
 
 function Hero({}: Props) {
   return (
-    <section className="relative min-h-[36rem] lg:min-h-[44rem]">
+    <section className="relative min-h-[36rem] lg:min-h-[44rem] ">
       <div className="absolute inset-0 bg-[url('/hero-img.avif')] bg-cover bg-center" />
 
       {/* adds dark overlay, over bg img */}
       <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/70 to-black/90" />
 
-      <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4">
-        <h1 className="text-5xl font-bold text-white">
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-y-4 px-4">
+        <h1 className="text-4xl text-center font-bold text-white lg:text-5xl">
           Unlimited movies, TV shows, and more
         </h1>
-        <p className="text-2xl text-white">Watch anywhere. For free.</p>
+        <p className="text-xl text-white lg:text-2xl">
+          Watch anywhere. For free.
+        </p>
 
         {/* <HeroSignUpForm /> */}
 
