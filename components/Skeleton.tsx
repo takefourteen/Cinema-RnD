478460d0583d1c555b32a673c3363795cfafcdec
Ismail <shaikhnagismail@gmail.com>
2023-09-21 06:30:01 +0200

    feat: add overlay to skeleton

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -1,31 +1,38 @@
 interface SkeletonProps {
   rows?: number;
   mainItemHeight?: number;
+  showOverlay?: boolean;
 }
 
-const Skeleton = ({ rows = 1, mainItemHeight = 50 }: SkeletonProps) => {
+const Skeleton = ({ rows = 1, mainItemHeight = 50, showOverlay = true }: SkeletonProps) => {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="relative flex h-full flex-1 flex-col">
       <div
-        className="w-full animate-pulse rounded-lg bg-slate-400"
+        className="h-full w-full animate-pulse rounded-lg bg-slate-400"
         style={{
           animationDuration: "1s",
           minHeight: `${mainItemHeight}px`,
         }}
       />
-      <div className="mt-4 flex flex-col gap-2">
-        {Array.from({ length: rows }, (_, i) => i + 1).map((_, i) => (
-          <div
-            key={i}
-            className="h-3 w-full animate-pulse rounded-lg bg-slate-300"
-            style={{
-              animationDelay: `${i * 0.1}s`,
-              animationDuration: "1s",
-              width: `${Math.floor(Math.random() * 71) + 30}%`, // Generates a random number between 30 and 100 (inclusive)
-            }}
-          />
-        ))}
-      </div>
+      {rows >= 1 && (
+        <div className="mt-4 flex flex-col gap-2">
+          {Array.from({ length: rows }, (_, i) => i + 1).map((_, i) => (
+            <div
+              key={i}
+              className="h-3 w-full animate-pulse rounded-lg bg-slate-300"
+              style={{
+                animationDelay: `${i * 0.1}s`,
+                animationDuration: "1s",
+                width: `${Math.floor(Math.random() * 71) + 30}%`, // Generates a random number between 30 and 100 (inclusive)
+              }}
+            />
+          ))}
+        </div>
+      )}
+      {/* overlay the image with a grain texture if rows are === 0 and showOver*/}
+      {rows === 0 && showOverlay && (
+        <div className="absolute inset-0 bg-[url('/grain-texture-image.svg')] opacity-30" />
+      )}
     </div>
   );
 };
