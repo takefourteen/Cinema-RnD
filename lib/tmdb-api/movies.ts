b4bad3891b721011718267d1cb7022073b561fb5
Ismail <shaikhnagismail@gmail.com>
2023-09-24 07:11:27 +0200

    refactor: migrate from axios to fetch

--- lib/tmdb-api/movies.ts
+++ lib/tmdb-api/movies.ts
@@ -1,5 +1,3 @@
-import axios, { AxiosResponse, AxiosError } from "axios";
-
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
@@ -8,25 +6,36 @@ interface MovieDetailsApiResponse {
   error: string | null;
 }
 
+/* 
+{"status_code":7,"status_message":"Invalid API key: You must be 
+granted a valid key.","success":false}
+*/
+
 export async function fetchMovieDetails(
   movieId: string,
 ): Promise<MovieDetailsApiResponse> {
   try {
-    const response: AxiosResponse<MovieDetailsData> = await axios.get(
+    const response = await fetch(
       `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US&append_to_response=credits`,
     );
 
+    if (!response.ok) {
+      // Parse the error response as JSON to extract status_message
+      const errorResponse = await response.json();
+      const errorMessage =
+        errorResponse?.status_message || "Unknown error occurred";
+      throw new Error(errorMessage);
+    }
+
+    const data = await response.json();
+
     return {
-      data: response.data,
+      data,
       error: null,
     };
   } catch (error) {
-    const axiosError = error as AxiosError;
-
-    // Handle different types of errors (e.g., network error, 404, 500, etc.)
-    const errorMessage = axiosError.response
-      ? `Error: ${axiosError.response.status} - ${axiosError.response.data}`
-      : `Network Error: ${axiosError.message}`;
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
 
     return {
       data: null,
