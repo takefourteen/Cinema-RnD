b4929fed320334b3c73907c84310151b7af3b37d
Ismail <shaikhnagismail@gmail.com>
2023-09-26 19:10:32 +0200

    refactor: new reusable btn

---
+++ components/DetailsButton.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-white text-black hover:bg-gray-200 dark:bg-slate-900 dark:text-slate-50 dark:hover:bg-slate-800",
+        outline: "border text-white hover:bg-white/10 dark:hover:bg-white/10",
+      },
+      size: {
+        default: "px-4 py-2",
+        sm: "px-4 py-2",
+        lg: "px-8 py-2",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+export interface DetailsButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const DetailsButton = React.forwardRef<HTMLButtonElement, DetailsButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+DetailsButton.displayName = "DetailsButton";
+
+export { DetailsButton, buttonVariants };
