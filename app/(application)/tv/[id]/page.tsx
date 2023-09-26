57adcb1c998d15227686ab59ed17790da403eb79
Ismail <shaikhnagismail@gmail.com>
2023-09-26 20:04:12 +0200

    update: feth data in tv page

--- app/(application)/tv/[id]/page.tsx
+++ app/(application)/tv/[id]/page.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
+import { fetchTvSeries } from "@/lib/tmdb-api/tv-series";
+
 type PageProps = {
   params: {
     id: string;
   };
 };
 
-const page = ({ params }: PageProps) => {
-  const { id } = params;
+const page = async ({ params: { id } }: PageProps) => {
+  // id from the params is a string with the tv series id and the tv series name seperated by a dash, so we split the string and get the id
+  const tvSeriesId = id.split("-")[0];
+
+  // fetch the tv series data
+  const { data: tvSeriesData, error } = await fetchTvSeries(tvSeriesId);
+
   return (
     <section className="master-container relative top-[70px] pb-[80px] pt-10 text-white">
-      <h1>Tv Page</h1>
-      <p>{id}</p>
+      <pre>{JSON.stringify(tvSeriesData, null, 2)}</pre>
     </section>
   );
 };
