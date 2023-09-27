466d4bffd524fa0c2a1f2f1754a3aa80c38cea99
Ismail <shaikhnagismail@gmail.com>
2023-09-27 06:58:59 +0200

    test: use suspense instead of onLoad

--- components/application-group/BackgroundPoster.tsx
+++ components/application-group/BackgroundPoster.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import { useState } from "react";
 import Image from "next/image";
+import { Suspense } from "react";
 
 import Skeleton from "@/components/Skeleton";
 
@@ -20,34 +18,29 @@ const BackgroundPoster = ({
   priority,
   imageClassNames,
 }: BackgroundPosterProps) => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  const handleImageLoad = () => {
-    setIsLoading(false);
-  };
-
   return (
     <>
-      {isLoading && (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <Skeleton rows={0} showOverlay={false} />{" "}
-        </div>
-      )}
-
       {/* show the backdrop_path img on larger screens or a div with an
         overlay color if backdrop_path is null
       */}
       <div className="hidden md:flex">
         {backdrop_path ? (
-          <Image
-            src={`${process.env.NEXT_PUBLIC_OG_TMBD_IMG_PATH}${backdrop_path}`}
-            alt={alt}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            priority={priority}
-            onLoad={handleImageLoad}
-            className={`absolute inset-0 bg-no-repeat ${imageClassNames}`}
-          />
+          <Suspense
+            fallback={
+              <div className="absolute inset-0 flex items-center justify-center">
+                <Skeleton rows={0} showOverlay={false} />{" "}
+              </div>
+            }
+          >
+            <Image
+              src={`${process.env.NEXT_PUBLIC_OG_TMBD_IMG_PATH}${backdrop_path}`}
+              alt={alt}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              priority={priority}
+              className={`absolute inset-0 bg-no-repeat ${imageClassNames}`}
+            />
+          </Suspense>
         ) : (
           <div className="absolute inset-0 bg-gradient-to-r from-black to-black"></div>
         )}
@@ -55,15 +48,22 @@ const BackgroundPoster = ({
 
       {/* show the poster_path img on smaller screens */}
       <div className="md:hidden">
-        <Image
-          src={`${process.env.NEXT_PUBLIC_OG_TMBD_IMG_PATH}${poster_path}`}
-          alt={alt}
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          priority={priority}
-          onLoad={handleImageLoad}
-          className={`absolute inset-0 bg-no-repeat ${imageClassNames}`}
-        />
+        <Suspense
+          fallback={
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Skeleton rows={0} showOverlay={false} />{" "}
+            </div>
+          }
+        >
+          <Image
+            src={`${process.env.NEXT_PUBLIC_OG_TMBD_IMG_PATH}${poster_path}`}
+            alt={alt}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority={priority}
+            className={`absolute inset-0 bg-no-repeat ${imageClassNames}`}
+          />
+        </Suspense>
       </div>
     </>
   );
