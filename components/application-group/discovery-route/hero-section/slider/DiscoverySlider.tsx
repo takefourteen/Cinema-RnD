89ae12b0fd37f9911f097582f1f7c53d077ffd94
Ismail <shaikhnagismail@gmail.com>
2023-09-28 07:00:01 +0200

    feat: infinite sroll capability

--- components/application-group/discovery-route/hero-section/slider/DiscoverySlider.tsx
+++ components/application-group/discovery-route/hero-section/slider/DiscoverySlider.tsx
@@ -23,33 +23,25 @@ const DiscoverySlider: React.FC<SliderProps> = ({ lengthOfList, children }) => {
   const listRef = useRef<HTMLUListElement | null>(null);
   const [slideNumber, setSlideNumber] = useState<number>(0);
   const [listWidth, setListWidth] = useState<number>(0);
-  const [screenWidth, setScreenWidth] = useState<number>(0);
 
   useEffect(() => {
     if (listRef.current) {
       setListWidth(listRef.current.scrollWidth);
-      setScreenWidth(window.innerWidth);
     }
-
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
   }, []);
 
   const handleLeftClick = (): void => {
-    if (slideNumber > 0) {
+    if (slideNumber === 0) {
+      setSlideNumber(lengthOfList - 1);
+    } else {
       setSlideNumber(slideNumber - 1);
     }
   };
 
   const handleRightClick = (): void => {
-    if (slideNumber < lengthOfList - 1) {
+    if (slideNumber === lengthOfList - 1) {
+      setSlideNumber(0);
+    } else {
       setSlideNumber(slideNumber + 1);
     }
   };
@@ -76,31 +68,11 @@ const DiscoverySlider: React.FC<SliderProps> = ({ lengthOfList, children }) => {
 
       {/* Slider Buttons */}
       <div className=" flex items-center gap-3 px-0 py-3 md:px-1 md:py-5 ">
-        <SliderButton
-          direction="left"
-          onClick={handleLeftClick}
-          disabled={slideNumber === 0 ? true : false}
-          style={{
-            display: `${slideNumber === 0 ? "none" : "flex"}`,
-          }}
-        >
+        <SliderButton direction="left" onClick={handleLeftClick}>
           <ChevronLeft className="text-xl" />
         </SliderButton>
 
-        <SliderButton
-          direction="right"
-          onClick={handleRightClick}
-          disabled={
-            slideNumber >= Math.ceil(listWidth / screenWidth) - 1 ? true : false
-          }
-          style={{
-            display: `${
-              slideNumber >= Math.ceil(listWidth / screenWidth) - 1
-                ? "none"
-                : "flex"
-            }`,
-          }}
-        >
+        <SliderButton direction="right" onClick={handleRightClick}>
           <ChevronRight className="text-xl" />
         </SliderButton>
       </div>
