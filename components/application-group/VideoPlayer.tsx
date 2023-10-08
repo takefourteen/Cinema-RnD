841c9ed01fd79fb24d6c1132c6e6df7b879c7988
Ismail <shaikhnagismail@gmail.com>
2023-10-08 20:32:01 +0200

    component to fetch and display the video

---
+++ components/application-group/VideoPlayer.tsx
@@ -0,0 +1,40 @@
+import { getVideoPlayerUrl } from "@/helpers/getVideoPlayerUrl";
+
+import LoadingSpinner from "../LoadingSpinner";
+
+interface VideoPlayerProps {
+  videoId: string;
+  isTmdb: number;
+  season: number;
+  episode: number;
+  className?: string;
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = async ({
+  videoId,
+  isTmdb,
+  season,
+  episode,
+  className,
+}) => {
+  // fetch the video player url
+  const playerUrl = await getVideoPlayerUrl(videoId, isTmdb, season, episode);
+
+  console.log("playerUrl", playerUrl);
+
+  // if the player url is not ready yet, show a loading spinner
+  if (!playerUrl) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <iframe
+      src={playerUrl}
+      frameBorder="0"
+      allowFullScreen
+      className={className}
+    />
+  );
+};
+
+export default VideoPlayer;
