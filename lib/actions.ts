4622dea2f61b1fe2866fa4627f2382346c204825
Ismail <shaikhnagismail@gmail.com>
2023-10-08 17:10:59 +0200

    return sorted results upon search

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,10 +1,13 @@
 "use server";
 
 import { searchAll } from "@/lib/tmdb-api/search";
-import { filterResultsByLanguage } from "./tmdb-api/filterResults";
+import { filterResultsByLanguage } from "@/helpers/filterResults";
+import { sortResultsByPopularity } from "@/helpers/sortResults";
 
 export async function fetchSearchResults(term: string, page: number = 1) {
   const results = await searchAll(term, page);
   const filteredResults = filterResultsByLanguage(results as any, "en");
-  return filteredResults;
+  const sortedResults = sortResultsByPopularity(filteredResults as any);
+
+  return sortedResults;
 }
