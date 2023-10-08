f522cbd16dd87b6f1d944b1e13fd3488d0c5c825
Ismail <shaikhnagismail@gmail.com>
2023-10-08 19:18:43 +0200

    add default value to istmdb

--- helpers/getVideoPlayerUrl.ts
+++ helpers/getVideoPlayerUrl.ts
@@ -1,25 +1,23 @@
 export async function getVideoPlayerUrl(
   videoId: string,
-  isTmdb: number,
+  isTmdb: number = 1,
   season: number,
   episode: number,
 ): Promise<string | null> {
+  const playerSettings = {
+    playerFont: "Arial",
+    playerBgColor: "#FFFFFF",
+    playerFontColor: "#000000",
+    playerPrimaryColor: "#FF0000",
+    playerSecondaryColor: "#00FF00",
+    playerLoader: 1,
+    preferredServer: 1,
+    playerSourcesToggleType: 1,
+  };
 
-    const playerSettings = {
-        playerFont: 'Arial',
-        playerBgColor: '#FFFFFF',
-        playerFontColor: '#000000',
-        playerPrimaryColor: '#FF0000',
-        playerSecondaryColor: '#00FF00',
-        playerLoader: 1,
-        preferredServer: 1,
-        playerSourcesToggleType: 1,
-    }
+  //   const requestUrl = `https://getsuperembed.link/?video_id=${videoId}&tmdb=${isTmdb}&season=${season}&episode=${episode}&player_font=${playerFont}&player_bg_color=${playerBgColor}&player_font_color=${playerFontColor}&player_primary_color=${playerPrimaryColor}&player_secondary_color=${playerSecondaryColor}&player_loader=${playerLoader}&preferred_server=${preferredServer}&player_sources_toggle_type=${playerSourcesToggleType}`;
 
-
-//   const requestUrl = `https://getsuperembed.link/?video_id=${videoId}&tmdb=${isTmdb}&season=${season}&episode=${episode}&player_font=${playerFont}&player_bg_color=${playerBgColor}&player_font_color=${playerFontColor}&player_primary_color=${playerPrimaryColor}&player_secondary_color=${playerSecondaryColor}&player_loader=${playerLoader}&preferred_server=${preferredServer}&player_sources_toggle_type=${playerSourcesToggleType}`;
-
-    const requestUrl = `https://getsuperembed.link/?video_id=${videoId}&tmdb=${isTmdb}&season=${season}&episode=${episode}&player_font=${playerSettings.playerFont}&player_bg_color=${playerSettings.playerBgColor}&player_font_color=${playerSettings.playerFontColor}&player_primary_color=${playerSettings.playerPrimaryColor}&player_secondary_color=${playerSettings.playerSecondaryColor}&player_loader=${playerSettings.playerLoader}&preferred_server=${playerSettings.preferredServer}&player_sources_toggle_type=${playerSettings.playerSourcesToggleType}`;
+  const requestUrl = `https://getsuperembed.link/?video_id=${videoId}&tmdb=${isTmdb}&season=${season}&episode=${episode}&player_font=${playerSettings.playerFont}&player_bg_color=${playerSettings.playerBgColor}&player_font_color=${playerSettings.playerFontColor}&player_primary_color=${playerSettings.playerPrimaryColor}&player_secondary_color=${playerSettings.playerSecondaryColor}&player_loader=${playerSettings.playerLoader}&preferred_server=${playerSettings.preferredServer}&player_sources_toggle_type=${playerSettings.playerSourcesToggleType}`;
 
   try {
     const response = await fetch(requestUrl);
