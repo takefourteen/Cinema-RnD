b0812296db01b1aa70eeafc1b9fd35154bca1d4f
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-09 11:02:49 +0000

    refactor

--- components/application-group/discovery-route/collections/CollectionCard.tsx
+++ components/application-group/discovery-route/collections/CollectionCard.tsx
@@ -7,31 +7,46 @@ import ImageLoader from "@/components/ImageLoader";
 type Props = {
   title: string;
   image: StaticImageData;
-  videoPath: string;
+  videoPath?: string; // Make videoPath optional
 };
+
+const CollectionCardImage = ({
+  image,
+  title,
+}: {
+  image: StaticImageData;
+  title: string;
+}) => (
+  <ImageLoader
+    loaderType="skeleton"
+    src={image}
+    sizes="(max-width: 768px) 200px, (max-width: 1024px) 225px, 250px,"
+    alt={title}
+    priority
+    className="absolute inset-0 z-20 h-full w-full object-contain p-4 brightness-75 lg:p-8"
+  />
+);
+
+const CollectionCardVideo = ({ videoPath }: { videoPath: string }) => (
+  <video
+    className="absolute h-full brightness-75"
+    autoPlay
+    loop
+    muted
+    preload="metadata"
+  >
+    <source src={`videos/collections/${videoPath}`} type="video/mp4" />
+  </video>
+);
 
 const CollectionCard = ({ image, videoPath, title }: Props) => {
   return (
-    <li className="group relative mb-4 h-auto w-[200px] ring-1 ring-white/30 transition-colors  hover:ring-white  md:w-[225px] lg:w-[250px]">
-      {/* <Link href={`/collection/${title.toLowerCase().replace(" ", "-")}`}> */}
+    <li className="group relative mb-4 h-auto w-[200px] ring-1 ring-white/30 transition-colors hover:ring-white md:w-[225px] lg:w-[250px]">
       <AspectRatio ratio={16 / 9}>
-        <ImageLoader
-          loaderType="skeleton"
-          src={image}
-          sizes="(max-width: 768px) 200px,
-                 (max-width: 1024px) 225px,
-                  250px,"
-          alt={title}
-          priority
-          className=" absolute inset-0 z-20 h-full w-full object-contain p-4 brightness-75 lg:p-8"
-        />
-
+        {videoPath && <CollectionCardVideo videoPath={videoPath} />}
         <div className="absolute inset-0 z-10 hidden bg-black/70 transition group-hover:bg-transparent md:block" />
-        <video className="absolute h-full brightness-75" autoPlay loop muted>
-          <source src={`videos/collections/${videoPath}`} type="video/mp4" />
-        </video>
+        <CollectionCardImage image={image} title={title} />
       </AspectRatio>
-      {/* </Link> */}
     </li>
   );
 };
