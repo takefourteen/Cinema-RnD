793bc2cdaa51aed2e00d22a4ed06a6f1da43900c
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-09 10:40:14 +0000

    refactor

--- app/(application)/watch-movie/[id]/page.tsx
+++ app/(application)/watch-movie/[id]/page.tsx
@@ -1,8 +1,20 @@
 import { Suspense } from "react";
+import dynamic from "next/dynamic";
 import Script from "next/script";
 
-import MovieExplorerPanel from "@/components/application-group/movie-route/middle-section/MovieExplorerPanel";
 import VideoPlayer from "@/components/application-group/VideoPlayer";
+import ExplorerPanel from "@/components/application-group/ExplorerPanel";
+
+// lazy load the following components
+const RecommendedMediaList = dynamic(
+  () =>
+    import(
+      "@/components/application-group/recommendations/RecommendedMediaList"
+    ),
+);
+const DetailsAboutShowSection = dynamic(
+  () => import("@/components/application-group/DetailsAboutShowSection"),
+);
 
 type PageProps = {
   params: {
@@ -13,6 +25,19 @@ type PageProps = {
 const page = ({ params: { id } }: PageProps) => {
   //  id from the params is a string with the movie id and the movie name seperated by a dash, so we split the string and get the id
   const movieId = id.split("-")[0];
+
+  const tabConfigs = [
+    {
+      key: "recommended",
+      title: "More Like This",
+      content: <RecommendedMediaList mediaId={movieId} mediaType="movie" />,
+    },
+    {
+      key: "details",
+      title: "Details",
+      content: <DetailsAboutShowSection mediaId={movieId} mediaType="movie" />,
+    },
+  ];
 
   return (
     <section className=" text-white">
@@ -28,11 +53,8 @@ const page = ({ params: { id } }: PageProps) => {
           />
         </Suspense>
       </div>
-
       {/* Middle Section */}
-      <MovieExplorerPanel mediaId={movieId} />
-
-      {/* Script */}
+      <ExplorerPanel tabConfigs={tabConfigs} />;{/* Script */}
       <Script src="../js/clearThePath.js" />
     </section>
   );
