61291e681883e481be4bee11b60dc7ec718ac8b4
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-10 14:38:18 +0200

    refactor

--- components/application-group/home-route/PopularMoviesSlider.tsx
+++ components/application-group/home-route/PopularMoviesSlider.tsx
@@ -1,22 +1,11 @@
-import {
-  getPopularTVShowsForPages,
-  getPopularMovies,
-} from "@/lib/tmdb-api/popular";
+import { fetchMultiplePagesOfPopularMovies } from "@/lib/tmdb-api/popular";
 
 import TrendingSlider from "../discovery-route/TrendingSlider";
 
 const PopularMoviesSlider = async () => {
-  const page = 1;
-  const region = "US";
-  const { data: popularMovies, error } = await getPopularMovies(page, region);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!popularMovies) {
-    return <div>No movies found</div>;
-  }
+  const popularMovies: PopularMovie[] = await fetchMultiplePagesOfPopularMovies(
+    1
+  );
 
   return (
     <TrendingSlider
