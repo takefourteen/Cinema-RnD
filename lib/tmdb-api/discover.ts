eb208be132fe78f32370feca5c247230b5e160cd
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-12 15:08:51 +0200

    add discover end point types

---
+++ lib/tmdb-api/discover.ts
@@ -0,0 +1,51 @@
+interface Genres {
+    [key: string]: {
+      [key: string]: number[];
+    };
+  }
+  
+  const genres: Genres = {
+    movies: {
+      latest: [],
+      actionAdventure: [28, 12],
+      anime: [16],
+      childrenFamily: [10751],
+      classic: [], // You might need a different approach for this, as "classic" isn't a standard genre tag
+      comedies: [35],
+      documentaries: [99],
+      dramas: [18],
+    },
+    tvSeries: {
+      latest: [],
+      actionAdventure: [10759],
+      anime: [16],
+      childrenFamily: [10751],
+      classic: [], // You might need a different approach for this, as "classic" isn't a standard genre tag
+      comedies: [35],
+      documentaries: [99],
+      dramas: [18],
+    },
+  };
+  
+  const baseMovieURL = 'https://api.themoviedb.org/3/discover/movie';
+  const baseTVSeriesURL = 'https://api.themoviedb.org/3/discover/tv';
+  
+  const generateAPIUrl = (category: string, type: string): string => {
+    const genreIds = genres[type][category].join(',');
+    const baseUrl = type === 'movies' ? baseMovieURL : baseTVSeriesURL;
+    return `${baseUrl}?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreIds}`;
+  };
+
+
+
+  
+  
+  
+  /* Example usage
+  const latestMoviesUrl = generateAPIUrl('latest', 'movies');
+  const latestTVSeriesUrl = generateAPIUrl('latest', 'tvSeries');
+  const actionAdventureMoviesUrl = generateAPIUrl('actionAdventure', 'movies');
+  const actionAdventureTVSeriesUrl = generateAPIUrl('actionAdventure', 'tvSeries');
+  */
+
+  
