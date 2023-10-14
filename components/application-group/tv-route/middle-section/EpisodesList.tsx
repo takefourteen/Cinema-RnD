cb4e368a89c455fc5e21928b33a592c92220c88b
Ismail <shaikhnagismail@gmail.com>
2023-10-14 16:34:26 +0200

    turn into a server component and use fetch()

--- components/application-group/tv-route/middle-section/EpisodesList.tsx
+++ components/application-group/tv-route/middle-section/EpisodesList.tsx
@@ -1,12 +1,6 @@
-"use client";
-
-import useSWR from "swr";
-
 import { fetchTvSeriesDetails } from "@/lib/tmdb-api/tv-series";
 
-import Skeleton from "@/components/Skeleton";
 import EpisodeListItem from "./EpisodeListItem";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import AnimatedStringLoader from "@/components/AnimatedStringLoader";
 
 type EpisodesListProps = {
@@ -14,58 +8,18 @@ type EpisodesListProps = {
   selectedSeason: number;
 };
 
-const EpisodesList = ({ tvSeriesId, selectedSeason }: EpisodesListProps) => {
+const EpisodesList = async ({
+  tvSeriesId,
+  selectedSeason,
+}: EpisodesListProps) => {
   const seasonNumber = selectedSeason.toString();
   const appendSeasonNumberToResponse = `season/${seasonNumber}`;
-  const {
-    data: seasonData,
-    error,
-    isLoading,
-  } = useSWR([tvSeriesId, seasonNumber], () =>
-    fetchTvSeriesDetails(tvSeriesId, 0, appendSeasonNumberToResponse),
+
+  const tvSeriesDetails = await fetchTvSeriesDetails(
+    tvSeriesId,
+    0,
+    appendSeasonNumberToResponse,
   );
-
-  // if there is an error, throw it
-  if (error) throw error;
-
-  // if there is no data, map over an array of 4 items
-  // and return a skeleton
-  if (!seasonData)
-    return (
-      <>
-        {/* <p className="font-body-text absolute right-0 top-2 flex gap-0.5 font-bold lg:top-4">
-          <AnimatedStringLoader loadingString="..." />
-          Episodes
-        </p> */}
-
-        <div className="grid  grid-cols-2  gap-x-4 gap-y-12 lg:grid-cols-3 xl:grid-cols-4">
-          {Array.from({ length: 4 }).map((_, index) => (
-            <div key={index} className="relative w-full ">
-              <AspectRatio ratio={16 / 9}>
-                <Skeleton rows={2} />
-              </AspectRatio>
-            </div>
-          ))}
-        </div>
-      </>
-    );
-
-  // if the data is loading, map over an array of 4 items
-  // and return a skeleton
-  if (isLoading)
-    return (
-      <>
-        <div className="grid  grid-cols-2  gap-x-4 gap-y-12 lg:grid-cols-3 xl:grid-cols-4">
-          {Array.from({ length: 4 }).map((_, index) => (
-            <div key={index} className="relative w-full ">
-              <AspectRatio ratio={16 / 9}>
-                <Skeleton rows={2} />
-              </AspectRatio>
-            </div>
-          ))}
-        </div>
-      </>
-    );
 
   return (
     <>
@@ -74,16 +28,16 @@ const EpisodesList = ({ tvSeriesId, selectedSeason }: EpisodesListProps) => {
       </p> */}
 
       <ul className="grid  grid-cols-2  gap-x-4 gap-y-12 lg:grid-cols-3 xl:grid-cols-4">
-        {(seasonData[appendSeasonNumberToResponse] as SeasonData).episodes.map(
-          (episode) => (
-            <EpisodeListItem
-              key={episode.id}
-              episodeData={episode}
-              tvSeriesId={tvSeriesId}
-              tvSeriesTitle={seasonData.original_name}
-            />
-          ),
-        )}
+        {(
+          tvSeriesDetails[appendSeasonNumberToResponse] as SeasonData
+        ).episodes.map((episode) => (
+          <EpisodeListItem
+            key={episode.id}
+            episodeData={episode}
+            tvSeriesId={tvSeriesId}
+            tvSeriesTitle={tvSeriesDetails.original_name}
+          />
+        ))}
       </ul>
     </>
   );
--- components/application-group/tv-route/middle-section/SeasonsAndEpisodes.tsx
+++ components/application-group/tv-route/middle-section/SeasonsAndEpisodes.tsx
@@ -3,9 +3,11 @@
 import { Suspense } from "react";
 import { useState } from "react";
 
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import SeasonSelect from "./SeasonSelect";
 import EpisodesList from "./EpisodesList";
 import LoadingSpinner from "@/components/LoadingSpinner";
+import Skeleton from "@/components/Skeleton";
 
 type SeasonsAndEpisodesProps = {
   tvSeriesId: string;
@@ -18,23 +20,32 @@ const SeasonsAndEpisodes = ({
   totalNumberOfSeasons,
   selectedSeasonFromUrl,
 }: SeasonsAndEpisodesProps) => {
-
-  const [selectedSeason, setSelectedSeason] = 
-  useState<number>(selectedSeasonFromUrl || 1);
+  const [selectedSeason, setSelectedSeason] = useState<number>(
+    selectedSeasonFromUrl || 1,
+  );
 
   return (
     <div className="relative flex flex-col gap-y-4 lg:gap-y-8">
-      <SeasonSelect 
-      numberOfSeasons={totalNumberOfSeasons} 
-      selectedSeason={selectedSeason}
-      setSelectedSeason={setSelectedSeason}
+      <SeasonSelect
+        numberOfSeasons={totalNumberOfSeasons}
+        selectedSeason={selectedSeason}
+        setSelectedSeason={setSelectedSeason}
       />
 
-      <Suspense fallback={<LoadingSpinner />}>
-        <EpisodesList
-         tvSeriesId={tvSeriesId} 
-          selectedSeason={selectedSeason}
-         />
+      <Suspense
+        fallback={
+          <div className="grid  grid-cols-2  gap-x-4 gap-y-12 lg:grid-cols-3 xl:grid-cols-4">
+            {Array.from({ length: 8 }).map((_, index) => (
+              <div key={index} className="relative w-full ">
+                <AspectRatio ratio={16 / 9}>
+                  <LoadingSpinner />
+                </AspectRatio>
+              </div>
+            ))}
+          </div>
+        }
+      >
+        <EpisodesList tvSeriesId={tvSeriesId} selectedSeason={selectedSeason} />
       </Suspense>
     </div>
   );
