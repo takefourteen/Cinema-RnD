291e16443225271387e653a05d7331ee9550335a
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-14 09:44:01 +0200

    update url

--- components/application-group/home-route/ShowDetailsLargeScreen.tsx
+++ components/application-group/home-route/ShowDetailsLargeScreen.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Suspense } from "react";
 
+import { slugify } from "@/helpers/slugify.ts";
 import { isMovieDetails } from "@/lib/tmdb-api/movies";
 import { fetchImages } from "@/lib/tmdb-api/images";
 
@@ -55,12 +56,17 @@ const ShowDetailsLargeScreen = async ({
     : movieOrTvShowDetails.first_air_date;
 
   // encoded link href to the details page of the movie or tv show
-  const detailsUrl = `/${type}/${movieOrTvShowDetails.id}-${encodeURIComponent(
-    movieOrTvShowTitle || "",
-  )}`;
+  // const detailsUrl = `/${type}/${movieOrTvShowDetails.id}-${encodeURIComponent(
+  //   movieOrTvShowTitle || "",
+  // )}`;
+  const detailsUrl = `/${type}/${slugify(movieOrTvShowTitle)}-${
+    movieOrTvShowDetails.id
+  }`;
 
   // play movie href to watch page of the movie or tv show
-  const playUrl = type === "movie" ? `/watch-movie/${movieOrTvShowDetails.id}-${encodeURIComponent( movieOrTvShowTitle || "")}` : `/watch-tv/${movieOrTvShowDetails.id}-${encodeURIComponent(movieOrTvShowTitle || "")}`;
+  const playUrl = `/watch-${type}/${slugify(movieOrTvShowTitle)}-${
+    movieOrTvShowDetails.id
+  }`;
 
   //   get the runtime for the movie or tv show in the format of 1h 30m, or 1h, or 30m
   let runtime;
