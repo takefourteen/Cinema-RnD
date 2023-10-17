f0f3397b128a48635bec51f6bbeee0988e96f547
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-17 16:37:08 +0200

    add some keywords

--- constants/keywords.ts
+++ constants/keywords.ts
@@ -1,8 +1,55 @@
 type Keyword = {
-    id: number;
-    name: string;
-}
+  id: number;
+  name: string;
+};
 
 export const keywords: Keyword[] = [
-    
-]
+  {
+    id: 316421,
+    name: "drama",
+  },
+  {
+    id: 322496,
+    name: "action",
+  },
+  {
+    id: 322268,
+    name: "comedy",
+  },
+  {
+    id: 286758,
+    name: "kids",
+  },
+  {
+    id: 18035,
+    name: "family",
+  },
+  {
+    id: 315058,
+    name: "horror",
+  },
+  {
+    id: 282080,
+    name: "documentary",
+  },
+  {
+    id: 210024,
+    name: "anime",
+  },
+  {
+    id: 293198,
+    name: "fantasy",
+  },
+  {
+    id: 9840,
+    name: "romance",
+  },
+  {
+    id: 321730,
+    name: "sci-fi",
+  },
+  {
+    id: 9716,
+    name: "stand-up comedy",
+  },
+];
