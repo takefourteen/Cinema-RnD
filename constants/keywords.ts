8e8b2000e6ac8e19b09d90725b9be7d66ec15fa7
Ismail <shaikhnagismail@gmail.com>
2023-10-18 06:36:42 +0200

    use typescript namespace for intellisense

--- constants/keywords.ts
+++ constants/keywords.ts
@@ -1,60 +1,15 @@
-type Keyword = {
-  id: number;
-  name: string;
-};
-
-export const keywords: Keyword[] = [
-    {
-      id: 322942,
-      name: "adventure"
-    },
-    {
-      id: 322496,
-      name: "action",
-    },
-    {
-      id: 210024,
-      name: "anime",
-    },
-    {
-      id: 322268,
-      name: "comedy",
-    },
-    {
-      id: 282080,
-      name: "documentary",
-    },
-    {
-      id: 316421,
-      name: "drama",
-    },
-    {
-      id: 18035,
-      name: "family",
-    },
-    {
-      id: 293198,
-      name: "fantasy",
-    },
-    {
-      id: 315058,
-      name: "horror",
-    },
-    {
-      id: 286758,
-      name: "kids",
-    },
-    {
-      id: 9840,
-      name: "romance",
-    },
-    {
-      id: 321730,
-      name: "sci-fi",
-    },
-    {
-      id: 9716,
-      name: "stand-up comedy",
-    },
-  ];
-  
+export namespace keywords {
+  export const adventure = 322942;
+  export const action = 322496;
+  export const anime = 210024;
+  export const comedy = 322268;
+  export const documentary = 282080;
+  export const drama = 316421;
+  export const family = 18035;
+  export const fantasy = 293198;
+  export const horror = 315058;
+  export const kids = 286758;
+  export const romance = 9840;
+  export const sciFi = 321730;
+  export const standUpComedy = 9716;
+}
