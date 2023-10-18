460a127ac2c0d7aae164fa7b7b7a8eadeda8f228
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-18 09:08:03 +0000

    new namespace file for list of genres

---
+++ constants/movieGenres.ts
@@ -0,0 +1,21 @@
+export namespace movieGenres {
+  export const action = 28;
+  export const adventure = 12;
+  export const animation = 16;
+  export const comedy = 35;
+  export const crime = 80;
+  export const documentary = 99;
+  export const drama = 18;
+  export const family = 10751;
+  export const fantasy = 14;
+  export const history = 36;
+  export const horror = 27;
+  export const music = 10402;
+  export const mystery = 9648;
+  export const romance = 10749;
+  export const sciFi = 878;
+  export const tvMovie = 10770;
+  export const thriller = 53;
+  export const war = 10752;
+  export const western = 37;
+}
---
+++ constants/tvSeriesGenres.ts
@@ -0,0 +1,19 @@
+export namespace tvSeriesGenres {
+    export const actionAdventure = 10759;
+    export const animation = 16;
+    export const comedy = 35;
+    export const crime = 80;
+    export const documentary = 99;
+    export const drama = 18;
+    export const family = 10751;
+    export const kids = 10762;
+    export const mystery = 9648;
+    export const news = 10763;
+    export const reality = 10764;
+    export const sciFiFantasy = 10765;
+    export const soap = 10766;
+    export const talk = 10767;
+    export const warPolitics = 10768;
+    export const western = 37;
+  }
+  
