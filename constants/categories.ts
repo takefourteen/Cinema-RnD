5f641f6e20ee1a8388663035dc380ba3d5e9b9dc
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-18 13:41:59 +0000

    add extensive list of movies and tv series

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,4 +1,6 @@
 import { keywords } from "@/constants/keywords";
+import { tvSeriesGenres } from "./tvSeriesGenres";
+import { movieGenres } from "./movieGenres";
 
 interface CategoryList {
   [key: string]: {
@@ -11,106 +13,232 @@ interface CategoryList {
 
 export const categories: CategoryList = {
   movies: {
-    actionAdventure: {
-      genreIds: [28, 12],
+    action: {
+      genreIds: [movieGenres.action],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.action}`,
+      },
+    },
+    adventure: {
+      genreIds: [movieGenres.adventure],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
         with_keywords: `${keywords.adventure}`,
       },
     },
-    anime: {
-      genreIds: [16],
+    animation: {
+      genreIds: [movieGenres.animation],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
+        with_keywords: `${keywords.animation}`,
       },
     },
-    childrenFamily: {
-      genreIds: [10751],
+    comedy: {
+      genreIds: [movieGenres.comedy],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
+        with_keywords: `${keywords.comedy}`,
       },
     },
-    classic: {
+    crime: {
+      genreIds: [movieGenres.crime],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.crime}`,
+      },
+    },
+    documentary: {
+      genreIds: [movieGenres.documentary],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.documentary}`,
+      },
+    },
+    drama: {
+      genreIds: [movieGenres.drama],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.drama}`,
+      },
+    },
+    family: {
+      genreIds: [movieGenres.family],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.family}`,
+      },
+    },
+    fantasy: {
+      genreIds: [movieGenres.fantasy],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.fantasy}`,
+      },
+    },
+    history: {
+      genreIds: [movieGenres.history],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.history}`,
+      },
+    },
+    horror: {
+      genreIds: [movieGenres.horror],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.horror}`,
+      },
+    },
+    music: {
+      genreIds: [movieGenres.music],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.music}`,
+      },
+    },
+    mystery: {
+      genreIds: [movieGenres.mystery],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.mystery}`,
+      },
+    },
+    romance: {
+      genreIds: [movieGenres.romance],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.romance}`,
+      },
+    },
+    sciFi: {
+      genreIds: [movieGenres.sciFi],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.sciFi}`,
+      },
+    },
+    standUpComedy: {
       genreIds: [],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
+        with_keywords: `${keywords.standUpComedy}`,
       },
     },
-    comedies: {
-      genreIds: [35],
+
+    tvMovie: {
+      genreIds: [movieGenres.tvMovie],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
       },
     },
-    documentaries: {
-      genreIds: [99],
+    thriller: {
+      genreIds: [movieGenres.thriller],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
+        with_keywords: `${keywords.thriller}`,
       },
     },
-    dramas: {
-      genreIds: [18],
+    western: {
+      genreIds: [movieGenres.western],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
-      },
-    },
-    topRated: {
-      genreIds: [],
-      filterAndSortOptions: {
-        sort_by: "vote_average.desc",
-        without_genres: "99,10755",
-        "vote_count.gte": 200,
+        with_keywords: `${keywords.western}`,
       },
     },
   },
+
   tvSeries: {
     actionAdventure: {
-      genreIds: [10759, 10762],
+      genreIds: [tvSeriesGenres.actionAdventure],
+      filterAndSortOptions: {
+        sort_by: `popularity.desc`,
+        with_keywords: `${keywords.action},${keywords.adventure}`,
+      },
+    },
+    animation: {
+      genreIds: [tvSeriesGenres.animation],
+      filterAndSortOptions: {
+        sort_by: `popularity.desc`,
+        with_keywords: `${keywords.animation}`,
+      },
+    },
+
+    comedy: {
+      genreIds: [tvSeriesGenres.comedy],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.comedy}`,
+      },
+    },
+    crime: {
+      genreIds: [tvSeriesGenres.crime],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.crime}`,
+      },
+    },
+    documentary: {
+      genreIds: [tvSeriesGenres.documentary],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.documentary}`,
+      },
+    },
+    drama: {
+      genreIds: [tvSeriesGenres.drama],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.drama}`,
+      },
+    },
+    family: {
+      genreIds: [tvSeriesGenres.family],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.family}`,
+      },
+    },
+    kids: {
+      genreIds: [tvSeriesGenres.kids],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.kids}`,
+      },
+    },
+    mystery: {
+      genreIds: [tvSeriesGenres.mystery],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.mystery}`,
+      },
+    },
+
+    reality: {
+      genreIds: [tvSeriesGenres.reality],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
       },
     },
-    anime: {
-      genreIds: [16],
+    sciFiFantasy: {
+      genreIds: [tvSeriesGenres.sciFiFantasy],
+      filterAndSortOptions: {
+        sort_by: "popularity.desc",
+        with_keywords: `${keywords.sciFi}`,
+      },
+    },
+    soap: {
+      genreIds: [tvSeriesGenres.soap],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
       },
     },
-    childrenFamily: {
-      genreIds: [10751],
+
+    western: {
+      genreIds: [tvSeriesGenres.western],
       filterAndSortOptions: {
         sort_by: "popularity.desc",
-      },
-    },
-    classic: {
-      genreIds: [],
-      filterAndSortOptions: {
-        sort_by: "popularity.desc",
-      },
-    },
-    comedies: {
-      genreIds: [35],
-      filterAndSortOptions: {
-        sort_by: "popularity.desc",
-      },
-    },
-    documentaries: {
-      genreIds: [99],
-      filterAndSortOptions: {
-        sort_by: "popularity.desc",
-      },
-    },
-    dramas: {
-      genreIds: [18],
-      filterAndSortOptions: {
-        sort_by: "popularity.desc",
-      },
-    },
-    topRated: {
-      genreIds: [],
-      filterAndSortOptions: {
-        sort_by: "vote_average.desc",
-        "vote_count.gte": 200,
+        with_keywords: `${keywords.western}`,
       },
     },
   },
