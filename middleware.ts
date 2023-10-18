7a0f6a2692a7415b99517336e6a7c52b82a2b8be
Ismail <shaikhnagismail@gmail.com>
2023-10-18 06:23:36 +0200

    use getServerSession to protect pages instead of middleware

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-export { default } from "next-auth/middleware";
-
-// Protect the /watch-tv and /watch-movie routes
-export const config = { 
-  matcher: ["/watch-tv/:path*", "/watch-movie/:path*"],
-};
