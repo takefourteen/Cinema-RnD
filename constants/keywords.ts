6615a42f52cce933295c5c8150d674bb7e78928b
Ismail Shaikhnag <80391089+PresidentIsmail@users.noreply.github.com>
2023-10-18 13:20:59 +0000

    add to keywords

--- constants/keywords.ts
+++ constants/keywords.ts
@@ -1,15 +1,22 @@
 export namespace keywords {
-  export const adventure = 322942;
   export const action = 322496;
+  export const adventure = 322942;
+  export const animation = 297442;
   export const anime = 210024;
   export const comedy = 322268;
+  export const crime = 323114;
   export const documentary = 282080;
   export const drama = 316421;
   export const family = 18035;
   export const fantasy = 293198;
+  export const history = 282633;
   export const horror = 315058;
+  export const music = 283297;
+  export const mystery = 316332;
   export const kids = 286758;
   export const romance = 9840;
   export const sciFi = 321730;
   export const standUpComedy = 9716;
+  export const thriller = 316362;
+  export const western = 305941;
 }
