99ec0c5a619b96b596702fb7eea43d7e93b2b918
Ismail <shaikhnagismail@gmail.com>
2023-10-19 05:47:07 +0200

    new function that sorts by vote count

--- helpers/sortResults.ts
+++ helpers/sortResults.ts
@@ -1,5 +1,7 @@
-export function sortResultsByPopularity<T extends { popularity: number }>(
+export const sortResultsByPopularity = <T extends { popularity: number }>(
   results: T[],
-) {
-  return results.sort((a, b) => b.popularity - a.popularity);
-}
+) => results.sort((a, b) => b.popularity - a.popularity);
+
+export const sortResultsByVoteCount = <T extends { vote_count: number }>(
+  results: T[],
+) => results.sort((a, b) => b.vote_count - a.vote_count);
