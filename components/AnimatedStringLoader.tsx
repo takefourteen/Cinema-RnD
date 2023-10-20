f22176af112d5a5c3e798d3bc24b6833cb5d3499
Ismail <shaikhnagismail@gmail.com>
2023-10-20 06:36:08 +0200

    add margin prop to loader

--- components/AnimatedStringLoader.tsx
+++ components/AnimatedStringLoader.tsx
@@ -2,13 +2,30 @@ import React from "react";
 
 type AnimatedStringLoaderProps = {
   loadingString: string;
+  margin?:
+    | "ml-0"
+    | "ml-1"
+    | "ml-2"
+    | "ml-3"
+    | "ml-4"
+    | "ml-5"
+    | "ml-6"
+    | "ml-7"
+    | "ml-8"
+    | "ml-9"
+    | "ml-10";
 };
 
-const AnimatedStringLoader = ({ loadingString }: AnimatedStringLoaderProps) => {
+const AnimatedStringLoader = ({
+  loadingString,
+  margin = "ml-1",
+}: AnimatedStringLoaderProps) => {
   return loadingString.split("").map((letter, i) => (
     <span
       key={i}
-      className={`animate-bounce  text-xl font-bold text-white lg:text-2xl`}
+      className={`animate-bounce space-x-10 text-xl font-bold text-white lg:text-2xl ${
+        i === 0 ? "" : margin
+      }`}
       style={{
         animationDelay: `${i * 0.1}s`,
         animationDuration: `1.5s`,
