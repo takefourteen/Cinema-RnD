50225e567ca6fe1bdbe5af8f6c87d945d7100901
Ismail <shaikhnagismail@gmail.com>
2023-10-22 20:56:08 +0200

    update styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,20 +4,20 @@ const Footer = () => {
   // get the current year
   const year = new Date().getFullYear();
   return (
-    <footer className="z-10 mb-[60px] mt-6  h-[50px] border-t border-gray-800 bg-transparent py-2 md:mb-0">
-      <div className="flex h-full items-center justify-center ">
+    <footer className=" z-10 mb-[60px] mt-6 h-[50px] border-t border-gray-800 bg-transparent py-2 md:mb-0">
+      <div className="font-small-text lg:font-extra-small-text flex h-full items-center justify-center ">
+        &copy; {year} &nbsp;
         <Link
           href={"https://ismailshaikhnag.vercel.app/"}
           target="_blank"
           rel="noopener noreferrer"
-          className=" inline-flex text-center text-sm tracking-wide text-gray-400 md:mt-0 lg:text-base "
+          className="inline-flex text-center tracking-wide text-gray-400 md:mt-0  "
         >
-          &copy; {year} &nbsp;
-          <span className="text-red-500 transition-colors hover:text-red-500/70 hover:underline">
-            Ismail Shaikhnag
+          <span className="text-primaryRed transition hover:text-primaryRed/70 ">
+            Ismail Shaikhnag.
           </span>
-          . All rights reserved.
         </Link>
+        &nbsp; All rights reserved.
       </div>
     </footer>
   );
