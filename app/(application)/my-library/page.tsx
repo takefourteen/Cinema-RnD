e3eeaee1a6552960cb74d466954ee35688cfaae2
Ismail <shaikhnagismail@gmail.com>
2023-10-22 17:27:17 +0200

    begin building my library page

--- app/(application)/my-library/page.tsx
+++ app/(application)/my-library/page.tsx
@@ -1,40 +1,11 @@
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
-
 import React from "react";
 
-const MyList = async () => {
-  // const { data: session, status } = useSession({
-  //   required: true,
-  //   onUnauthenticated() {
-  //     redirect("/login?callbackUrl=/my-library");
-  //   },
-  // });
-  // const user = session?.user as UserData;
-
-  const session = await getServerSession(authOptions);
-  const user = session?.user as UserData;
-  console.log("user", user);
-  console.log("session", session);
-
-  if (!user) {
-    redirect("/login?callbackUrl=/my-library");
-  }
-
+const page = () => {
   return (
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-2xl font-bold">
-          This is a <span className="text-emerald-500">client-side</span>{" "}
-          protected page
-        </h1>
-        <h2 className="mt-4 font-medium">You are logged in as:</h2>
-        <p className="mt-4">{user?.firstName}</p>
-      </div>
+    <section className="master-container relative mt-[70px] pb-[80px] pt-10 lg:mt-[90px]">
+      my library
     </section>
   );
 };
 
-export default MyList;
+export default page;
