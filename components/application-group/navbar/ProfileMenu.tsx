efa4fa28b11b6fa6f198684f6f8b4b98efcc7b0a
Ismail <shaikhnagismail@gmail.com>
2023-10-22 06:56:32 +0200

    build new profile menu to replace auth btns

--- components/application-group/navbar/ProfileMenu.tsx
+++ components/application-group/navbar/ProfileMenu.tsx
@@ -1,160 +1,116 @@
 "use client";
 
+import { Fragment } from "react";
 import Link from "next/link";
-import { useState, useEffect, useRef, ReactNode } from "react";
 
-import { HiMenuAlt4 as HamburgerMenuIcon } from "react-icons/hi";
-import { IoMdClose } from "react-icons/io";
-import { Button } from "../../ui/button";
-import { Separator } from "../../ui/separator";
+import { ProfileIcon } from "@/components/ui/icons/Icons";
+import profileSignedIn from "@/assets/images/placeholders/profile-signed-in.svg";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+
+export type UserProfileData = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+};
 
 type ProfileMenuProps = {
-  onDarkenBackground: () => void;
-  showLogOutBtn: boolean;
-  logOutBtn: ReactNode;
-  logInBtn: ReactNode;
+  userData: UserProfileData | null;
+  toggleNavBarBgColor: () => void;
+  onSignOut: () => Promise<void>;
+  callbackUrl: string;
 };
 
 const ProfileMenu = ({
-  onDarkenBackground,
-  showLogOutBtn,
-  logOutBtn,
-  logInBtn,
+  userData,
+  toggleNavBarBgColor,
+  onSignOut,
+  callbackUrl,
 }: ProfileMenuProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement | null>(null);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-    onDarkenBackground(); // darken the background when the menu is open
-  };
-
-  // Event listener to close the menu when the user clicks outside of it
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
-        onDarkenBackground(); // darken the background when the menu is open
-      }
-    };
-
-    if (isOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    }
-
-    // cleanup the event listener when the component unmounts
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpen, onDarkenBackground]);
-
-  // Event listener to close the menu when the user presses the escape key
-  useEffect(() => {
-    const handleEscKey = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        setIsOpen(false);
-      }
-    };
-
-    if (isOpen) {
-      document.addEventListener("keydown", handleEscKey);
-    }
-
-    // cleanup the event listener when the component unmounts
-    return () => {
-      document.removeEventListener("keydown", handleEscKey);
-    };
-  }, [isOpen]);
-
-  // close the menu when the user clicks on a link
-  const handleNavLinkClick = () => {
-    setIsOpen(false);
-    onDarkenBackground();
-  };
-
   return (
-    <div className="lg:hidden" ref={menuRef}>
-      {/* Hamburger and Close Menu Btn */}
-      <button
-        className={`mr-4 block rounded-full text-white transition-colors hover:bg-white/10`}
-        onClick={toggleMenu}
-        aria-label="Toggle menu"
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        className="relative h-[40px] w-[40px] rounded-full p-0 hover:bg-transparent hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white focus-visible:ring-offset-1 lg:h-[48px] lg:w-[48px]"
+        onClick={toggleNavBarBgColor}
       >
-        <div
-          className={` transform p-2 transition-transform ${
-            isOpen ? "rotate-90" : ""
-          }`}
-        >
-          {isOpen ? (
-            <IoMdClose className={`h-6 w-6 sm:h-8 sm:w-8`} />
-          ) : (
-            <HamburgerMenuIcon className={`h-6 w-6 sm:h-8 sm:w-8`} />
-          )}
-        </div>
-      </button>
+        {userData ? (
+          <Image
+            src={profileSignedIn}
+            alt="profile"
+            fill
+            sizes="(max-width: 1024px) 40px, 48px"
+            className="rounded-full object-cover ring-1 ring-white ring-offset-1 "
+          />
+        ) : (
+          <ProfileIcon />
+        )}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 py-2" align="end" forceMount>
+        {userData && (
+          <Fragment>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-2">
+                {/* display the users name if it exists */}
+                {userData.firstName && (
+                  <p className="font-semibold leading-none">
+                    {userData.firstName} {userData.lastName}
+                  </p>
+                )}
 
-      {/* Menu */}
-      {isOpen && (
-        <>
-          <ul className="absolute left-0 right-0 top-[75px] z-50 overflow-hidden bg-black/90 px-6  py-6 shadow-md transition-all md:right-auto lg:top-[90px]">
-            <li className=" px-10 hover:bg-[#40445999]">
-              <NavLink href="/" onClick={handleNavLinkClick}>
-                Home
-              </NavLink>
-            </li>
-            <li className="mt-4 px-10  hover:bg-[#40445999]">
-              <NavLink href="#" onClick={handleNavLinkClick}>
-                movies
-              </NavLink>
-            </li>
-            <li className="mt-4 px-10 hover:bg-[#40445999]">
-              <NavLink href="#" onClick={handleNavLinkClick}>
-                Tv Series
-              </NavLink>
-            </li>
-            <li className="mt-4 px-10 hover:bg-[#40445999]">
-              <NavLink href="#" onClick={handleNavLinkClick}>
-                Categories
-              </NavLink>
-            </li>
-            <li className="mt-4 px-10 hover:bg-[#40445999]">
-              <NavLink href="/my-library" onClick={handleNavLinkClick}>
-                my library
-              </NavLink>
-            </li>
-            {/* separator line and then login that goes to /login */}
-            <Separator className="mt-2 bg-white/40" />
-            <li className="mt-2 px-10 hover:bg-[#40445999]">
-              {showLogOutBtn ? logOutBtn : logInBtn}
-            </li>
-          </ul>
+                {/* display the users email if it exists */}
+                {userData.email && (
+                  <p className="text-xs leading-none text-gray-600">
+                    {userData.email}
+                  </p>
+                )}
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+          </Fragment>
+        )}
 
-          {/*dark overlay on menu open */}
-          {/* <div className="absolute inset-0 min-h-screen bg-black/20"></div> */}
-        </>
-      )}
-    </div>
-  );
-};
+        {/* display the sign out button if the user is signed in */}
+        {userData && (
+          <DropdownMenuItem onClick={onSignOut} className="cursor-pointer">
+            Log out
+          </DropdownMenuItem>
+        )}
 
-// component for displaying navbar links
-type NavLinkProps = {
-  href: string;
-  children: React.ReactNode;
-  onClick: () => void;
-};
+        {/* display the log in and create acount button if the user is not signed in */}
+        {!userData && (
+          <Fragment>
+            <DropdownMenuItem className="font-small-text cursor-pointer font-semibold text-primaryRed focus:bg-slate-800 focus:text-primaryRed">
+              <Link
+                href={`/signup?callbackUrl=${encodeURIComponent(callbackUrl)}`}
+                className="flex w-full"
+              >
+                Create Account
+              </Link>
+            </DropdownMenuItem>
 
-const NavLink = ({ href, onClick, children }: NavLinkProps) => {
-  return (
-    <Button
-      asChild
-      variant="ghost"
-      className="flex h-fit w-full justify-start rounded-lg px-[10px] py-[10px] text-sm font-bold uppercase text-white hover:bg-transparent hover:text-white"
-    >
-      <Link href={`${href}`} onClick={onClick}>
-        {children}
-      </Link>
-    </Button>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem className="font-small-text cursor-pointer">
+              <Link
+                href={`/login?callbackUrl=${encodeURIComponent(callbackUrl)}`}
+                className="flex w-full"
+              >
+                Log In
+              </Link>
+            </DropdownMenuItem>
+          </Fragment>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
