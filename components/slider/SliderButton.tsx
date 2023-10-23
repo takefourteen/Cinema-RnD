07fa9e10cc2cfe528ab59b2dfa36bd3926a64947
Ismail <shaikhnagismail@gmail.com>
2023-10-23 07:09:05 +0200

    update styling

--- components/slider/SliderButton.tsx
+++ components/slider/SliderButton.tsx
@@ -16,7 +16,7 @@ const SliderButton = ({
     <DetailsButton
       variant={"outline"}
       size={"icon"}
-      className={`h-9  w-9 rounded-full  border-white bg-white  text-white hover:bg-white/70 md:h-10 md:w-10 ${className}`}
+      className={`h-9  w-9 rounded-full  border-white bg-white  text-white hover:border-white/90 hover:bg-white/90 md:h-10 md:w-10 lg:h-12 lg:w-12 ${className}`}
       onClick={handleClick}
       {...props}
     >
