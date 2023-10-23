44135fc4bcb9917936301d0c9cbd6d0f3e367f10
Ismail <shaikhnagismail@gmail.com>
2023-10-23 06:36:16 +0200

    begin building out the library

--- app/(application)/library/page.tsx
+++ app/(application)/library/page.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import {
+  fetchMultiplePagesOfPopularMovies,
+  fetchMultiplePagesOfPopularTvSeries,
+} from "@/lib/tmdb-api/popular";
 
-const page = () => {
+import RenderSlider from "@/components/slider/RenderSlider";
+
+const page = async () => {
+  const popularMoviesPromise = fetchMultiplePagesOfPopularMovies(1);
+  const popularTvSeriesPromise = fetchMultiplePagesOfPopularTvSeries(1);
+
+  const [popularMovies, popularTvSeries] = await Promise.all([
+    popularMoviesPromise,
+    popularTvSeriesPromise,
+  ]);
+
   return (
-    <section className="master-container relative mt-[70px] pb-[80px] pt-10 lg:mt-[90px]">
-      library
+    <section className=" relative mt-[70px] pb-[80px] pt-10 lg:mt-[90px]">
+      <h1 className="font-header-2 master-container w-max border-b-4 border-b-primaryRed capitalize">
+        my list
+      </h1>
+
+      <RenderSlider sectionTitle="Movies" sliderData={popularMovies} />
     </section>
   );
 };
