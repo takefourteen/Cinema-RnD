5910932aed8e13cd190bdd879527eb7e6ed4ba01
Ismail <shaikhnagismail@gmail.com>
2023-10-24 05:10:33 +0200

    refactor

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,17 +3,11 @@
 import { searchAll } from "@/lib/tmdb-api/search";
 import { filterResults } from "@/helpers/filterResults";
 import { sortResultsByPopularity } from "@/helpers/sortResults";
-import { filterMediaWithVideoUrl } from "@/helpers/filterMediaWithVideoUrl";
 
 export async function fetchSearchResults(term: string, page: number = 1) {
   const results = await searchAll(term, page);
-
   const filteredResults = filterResults(results as any, "en");
-
   const sortedResults = sortResultsByPopularity(filteredResults as any);
-  const resultsWithVideoUrl = await filterMediaWithVideoUrl(
-    sortedResults as any,
-  );
 
-  return resultsWithVideoUrl;
+  return sortedResults;
 }
