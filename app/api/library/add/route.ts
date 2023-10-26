77f9bd8108de9cf6b0403bf3ce0be5f16effb46e
Ismail <shaikhnagismail@gmail.com>
2023-10-26 06:59:04 +0200

    implement add to library backend

--- app/api/library/add/route.ts
+++ app/api/library/add/route.ts
@@ -16,16 +16,29 @@ interface LibraryItem {
   title: string;
 }
 
+interface LibraryResponse {
+  message: string;
+  library?: LibraryItem[];
+}
+
+interface LibraryResponse {
+  message: string;
+  library?: LibraryItem[];
+}
+
 export const POST = async (req: NextRequest): Promise<NextResponse> => {
   try {
-    const { mediaId, mediaType, mediaTitle } = await req.json() as Media;
+    const { mediaId, mediaType, mediaTitle } = (await req.json()) as Media;
 
     // Ensure the user is authenticated and has the necessary role
     const session = await getServerSession(authOptions);
 
     // if there is no session, return an error message and status code letting the user know they are not authenticated
-    if (!session) {
-      return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+    if (!session?.user) {
+      const response: LibraryResponse = {
+        message: "Not authenticated",
+      };
+      return NextResponse.json(response, { status: 401 });
     }
 
     // Connect to the database
@@ -36,22 +49,36 @@ export const POST = async (req: NextRequest): Promise<NextResponse> => {
 
     // Check if the item is already in the user's library to prevent duplicates
     const existingItem = user.library.find(
-      (item: LibraryItem) => item.id === mediaId && item.type === mediaType,
+      (item: LibraryItem) => item.id == mediaId && item.type === mediaType,
     );
+
     if (existingItem) {
-      return NextResponse.json({ error: "Item already in the library" }, { status: 422 });
+      const response: LibraryResponse = {
+        message: `This ${
+          mediaType === "movie" ? "movie" : "TV show"
+        } is already in your library`,
+      };
+      return NextResponse.json(response, { status: 422 });
     }
 
     // Add the new item to the user's library
     const result = await User.findOneAndUpdate(
       { email: session.user?.email },
-      { $push: { library: { id: mediaId, type: mediaType, name: mediaTitle } } },
+      {
+        $push: { library: { id: mediaId, type: mediaType, title: mediaTitle } },
+      },
       { new: true },
     );
-
-    return NextResponse.json({ success: true });
+    const response: LibraryResponse = {
+      message: "Item added to library",
+      library: result.library,
+    };
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     console.error("Error adding item to library:", error);
-    return NextResponse.json({ error: "Error adding item to library" }, { status: 500 });
+    const response: LibraryResponse = {
+      message: "Error adding item to library",
+    };
+    return NextResponse.json(response, { status: 500 });
   }
 };
