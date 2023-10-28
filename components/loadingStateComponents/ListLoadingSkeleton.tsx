d4dbec2cda75a271922205da7166227594a86d85
Ismail <shaikhnagismail@gmail.com>
2023-10-28 18:07:19 +0200

    update styling

--- components/loadingStateComponents/ListLoadingSkeleton.tsx
+++ components/loadingStateComponents/ListLoadingSkeleton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const ListLoadingSkeleton = () => {
   return (
-    <div className="grid  gap-x-4  gap-y-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid  grid-cols-2  gap-x-4 gap-y-12 lg:grid-cols-3 xl:grid-cols-4">
       {Array.from({ length: 8 }).map((_, index) => (
         <div key={index} className="relative w-full ">
           <AspectRatio ratio={16 / 9}>
