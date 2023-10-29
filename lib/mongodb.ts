f5abfff88078e96765cbba2544cf731c89f8cb3d
Ismail <shaikhnagismail@gmail.com>
2023-10-29 07:05:11 +0200

    singleton pattern to manage the database connection and ensure that you only connect once

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,28 @@
-// use mongoose to connect to mongodb
 import mongoose from "mongoose";
 
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
+    "Please define the MONGODB_URI environment variable inside .env.local",
   );
 }
 
+let isConnected = false;
+
 export const connectToDatabase = async () => {
-    try {
-        await mongoose.connect(uri);
-        console.log("Successfully connected to mongodb");
-    } catch (error) {
-        console.log("Error connecting to mongodb", error);
-    }
-}
+  console.log("is there a connection?", isConnected);
+  if (isConnected) {
+    // If the connection is already established, return immediately.
+    return;
+  }
+
+  try {
+    await mongoose.connect(uri);
+    isConnected = true;
+    console.log("Successfully connected to mongodb");
+  } catch (error) {
+    console.error("Error connecting to mongodb", error);
+    throw error;
+  }
+};
