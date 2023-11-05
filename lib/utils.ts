726598b8f6da71f415a88f1b95b5698a0811debf
Ismail <shaikhnagismail@gmail.com>
2023-11-05 23:26:36 +0200

    Fix formatting and simplify pagination logic in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const generatePagination = (currentPage: number, totalPages: number) => {
@@ -13,27 +13,19 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
   }
 
   // If the current page is among the first 3 pages,
-  // show the first 3, an ellipsis, and the last 2 pages.
-  if (currentPage <= 3) {
-    return [1, 2, 3, "...", totalPages - 1, totalPages];
+  // show the first 3, and the last page.
+  if (currentPage <= 2) {
+    return [1, 2, 3, totalPages];
   }
 
-  // If the current page is among the last 3 pages,
-  // show the first 2, an ellipsis, and the last 3 pages.
-  if (currentPage >= totalPages - 2) {
-    return [1, 2, "...", totalPages - 2, totalPages - 1, totalPages];
+  // If the current page is among the last 2 pages,
+  // show the first page and the last 3.
+  if (currentPage >= totalPages - 1) {
+    return [1,  totalPages - 2, totalPages - 1, totalPages];
   }
 
   // If the current page is somewhere in the middle,
-  // show the first page, an ellipsis, the current page and its neighbors,
-  // another ellipsis, and the last page.
-  return [
-    1,
-    "...",
-    currentPage - 1,
-    currentPage,
-    currentPage + 1,
-    "...",
-    totalPages,
-  ];
+  // show the first page, the current page and its neighbors,
+  // and the last page.
+  return [1, currentPage - 1, currentPage, currentPage + 1, totalPages];
 };
