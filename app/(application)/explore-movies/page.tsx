c2bc6382a72a9d11441d6a8b312be4fca4ecd684
Ismail <shaikhnagismail@gmail.com>
2023-11-05 21:53:21 +0200

    Add movie genres and display filtered and sorted content

--- app/(application)/explore-movies/page.tsx
+++ app/(application)/explore-movies/page.tsx
@@ -1,49 +1,125 @@
 import { Suspense } from "react";
 
+import { movieGenres } from "@/constants/movieGenres";
+
+import LoadingSpinner from "@/components/skeletons/LoadingSpinner";
+import FilteredAndSortedMediaList from "@/components/application-group/explore-route/FilteredAndSortedMediaList";
+
 // import ComingSoon from "@/components/ui/ComingSoon"
 import GenreSelect from "@/components/application-group/explore-route/GenreSelect";
 
-const genres: { title: string }[] = [
+const genres: { title: string; id: number }[] = [
   {
     title: "Action",
-  },
-  {
-    title: "Animation",
+    id: movieGenres.action,
   },
   {
     title: "Adventure",
+    id: movieGenres.adventure,
+  },
+  {
+    title: "Animation",
+    id: movieGenres.animation,
   },
   {
     title: "Comedy",
+    id: movieGenres.comedy,
+  },
+  {
+    title: "Crime",
+    id: movieGenres.crime,
   },
   {
     title: "Documentary",
+    id: movieGenres.documentary,
   },
   {
     title: "Drama",
+    id: movieGenres.drama,
+  },
+  {
+    title: "Family",
+    id: movieGenres.family,
+  },
+  {
+    title: "Fantasy",
+    id: movieGenres.fantasy,
+  },
+  {
+    title: "History",
+    id: movieGenres.history,
   },
   {
     title: "Horror",
+    id: movieGenres.horror,
+  },
+  {
+    title: "Music",
+    id: movieGenres.music,
+  },
+  {
+    title: "Mystery",
+    id: movieGenres.mystery,
   },
   {
     title: "Romance",
+    id: movieGenres.romance,
   },
   {
-    title: "Sci-Fi & Fantasy",
+    title: "Sci-Fi",
+    id: movieGenres.sciFi,
+  },
+  {
+    title: "TV Movie",
+    id: movieGenres.tvMovie,
+  },
+  {
+    title: "Thriller",
+    id: movieGenres.thriller,
+  },
+  {
+    title: "War",
+    id: movieGenres.war,
+  },
+  {
+    title: "Western",
+    id: movieGenres.western,
   },
 ];
 
-const page = () => {
+type Props = {
+  searchParams: {
+    genres?: string | undefined;
+  };
+};
+
+const page = ({ searchParams }: Props) => {
+  const urlGenres = searchParams.genres?.split("~") as string[] | null;
+
+  // convert the string array to number array, where the genres match their given id
+  let urlGenresAsIds: number[] | null = null;
+
+  if (urlGenres && urlGenres.length > 0) {
+    urlGenresAsIds = urlGenres
+      .map((genre) => {
+        return genres.find((genreObj) => genreObj.title === genre)?.id;
+      })
+      .filter(Boolean) as number[];
+  }
+
   return (
     <section className="master-container relative mt-[72px] w-full pb-[80px] pt-10 lg:mt-[92px]">
       <div className="flex items-center gap-x-8 md:gap-x-12">
         <h1 className="text-3xl font-bold md:text-4xl">Movies</h1>
         <Suspense fallback={null}>
-          <GenreSelect genres={genres} />
+          <GenreSelect genres={genres} urlGenres={urlGenres} />
         </Suspense>
-
-        {/* Display all the filtered and sorted content */}
       </div>
+
+      {/* Display all the filtered and sorted content */}
+      <Suspense fallback={<LoadingSpinner />}>
+        <FilteredAndSortedMediaList urlGenres={urlGenresAsIds} />
+      </Suspense>
     </section>
   );
 };
