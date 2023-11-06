beefa3f65e39615155a7e58af9105229d88d6701
Ismail <shaikhnagismail@gmail.com>
2023-11-06 20:44:07 +0200

    Add ImageLoader component to ImdbRating for better image loading

--- components/application-group/ImdbRating.tsx
+++ components/application-group/ImdbRating.tsx
@@ -1,18 +1,28 @@
-import Image from "next/image";
-
 import imdbImg from "@/assets/images/imdb-1.svg";
+import ImageLoader from "../ImageLoader";
 
 type Props = {
   rating: number;
+  showFullRating?: boolean;
+  size?: "default" | "small";
 };
 
-const ImdbRating = ({ rating }: Props) => {
+const ImdbRating = ({ rating, showFullRating, size = "default" }: Props) => {
+  const imgSize = size === "default" ? 40 : 30;
+
   return (
     <div className="flex items-center gap-1">
-      <Image src={imdbImg} alt="imdb logo" width={40} />
-      <span className="font-semibold  tracking-wide text-white">
+      {/* <Image src={imdbImg} alt="imdb logo" width={40} /> */}
+      <ImageLoader
+        loaderType="skeleton"
+        src={imdbImg}
+        alt="imdb logo"
+        width={imgSize}
+      />
+      <span className="font-small-text font-semibold  tracking-wide text-white">
         {rating}
-        <span className="text-sm text-gray-400">/10</span>
+
+        {showFullRating && <span className="text-sm text-gray-400">/10</span>}
       </span>
     </div>
   );
