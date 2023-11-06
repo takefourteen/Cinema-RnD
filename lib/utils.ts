d099baf4157c55c348aa00e3caca27103bdd1592
Ismail <shaikhnagismail@gmail.com>
2023-11-06 18:25:30 +0200

    Update generatePagination function to display all pages without ellipsis if total number of pages is 5 or less.

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const generatePagination = (currentPage: number, totalPages: number) => {
-  // If the total number of pages is 7 or less,
+  // If the total number of pages is 5 or less,
   // display all pages without any ellipsis.
-  if (totalPages <= 7) {
+  if (totalPages <= 5) {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
 
@@ -21,7 +21,7 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
   // If the current page is among the last 2 pages,
   // show the first page and the last 3.
   if (currentPage >= totalPages - 1) {
-    return [1,  totalPages - 2, totalPages - 1, totalPages];
+    return [1, totalPages - 2, totalPages - 1, totalPages];
   }
 
   // If the current page is somewhere in the middle,
