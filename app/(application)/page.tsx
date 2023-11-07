6ff4b277be2aadf2dcb1416e020544f7db3e1c97
Ismail <shaikhnagismail@gmail.com>
2023-11-07 20:19:12 +0200

    Add trending movies and TV shows to home page slider

--- app/(application)/page.tsx
+++ app/(application)/page.tsx
@@ -1,21 +1,46 @@
 import { Suspense } from "react";
 
-import LoadingSpinner from "@/components/skeletons/LoadingSpinner";
+import { fetchMovieDetails } from "@/lib/tmdb-api/movies";
+import { fetchTvSeriesDetails } from "@/lib/tmdb-api/tv-series";
+import { fetchTrendingMovies } from "@/lib/tmdb-api/trending";
+import { fetchTrendingTVSeries } from "@/lib/tmdb-api/trending";
+
+import DiscoverySlider from "@/components/application-group/home-route/hero-section/slider/DiscoverySlider";
+import DiscoveryHeroSectionSliderBody from "@/components/application-group/home-route/hero-section/slider/DiscoveryHeroSectionSliderBody";
 import YourLibrary from "@/components/application-group/home-route/YourLibrary";
-import StreamingServicesSlideShow from "@/components/application-group/home-route/streaming-services/StreamingServicesSlideShow";
-import CollectionsSlideShow from "@/components/application-group/home-route/collections/CollectionsSlideShow";
-import ColorFulBanner from "@/components/application-group/legecy-home-route/ColorFulBanner";
-import HomeHeroSection from "@/components/application-group/home-route/hero-section/HomeHeroSection";
-import CategoriesSlider from "@/components/application-group/home-route/hero-section/slider/CategoriesSlider";
 import AnimatedStringLoader from "@/components/skeletons/AnimatedStringLoader";
 import DataFetchingMediaCardSkeleton from "@/components/skeletons/DataFetchingMediaCardSkeleton";
+import CategoriesSlider from "@/components/application-group/home-route/hero-section/slider/CategoriesSlider";
 
 // ===================================
 // Time-based Revalidation in Next.js
 // ===================================
 export const revalidate = 3600 * 24; // 24 hours
 
-const page = () => {
+const page = async () => {
+  // fetch trending movies and tv shows
+  const [trendingMovies, trendingTVSeries] = await Promise.all([
+    fetchTrendingMovies(),
+    fetchTrendingTVSeries(),
+  ]);
+
+  // mix the movies and tv shows together into one array. only 2 of each
+  const mixedTrending: (TrendingMovie | TrendingTVSeries)[] = [];
+  for (let i = 0; i < 2; i++) {
+    mixedTrending.push(trendingMovies[i]);
+    mixedTrending.push(trendingTVSeries[i]);
+  }
+
+  // fetch details for each movie and tv show
+  const movieAndTvShowDetailsPromise = mixedTrending.map((item) => {
+    return item.media_type === "movie"
+      ? fetchMovieDetails(item.id)
+      : fetchTvSeriesDetails(item.id);
+  });
+
+  // Wait for all promises to resolve
+  const movieAndTvShowDetails = await Promise.all(movieAndTvShowDetailsPromise);
+
   return (
     <section>
       {/*
@@ -23,15 +48,16 @@ const page = () => {
         Hero Section 
         -----------
        */}
-      <Suspense
-        fallback={
-          <div className=" h-[70dvh] w-[100vw] flex-1 md:h-[75dvh]">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        <HomeHeroSection />
-      </Suspense>
+      <DiscoverySlider lengthOfList={movieAndTvShowDetails.length}>
+        <ul className="flex gap-x-0">
+          {movieAndTvShowDetails.map((item) => (
+            <DiscoveryHeroSectionSliderBody
+              key={item.id}
+              movieOrTvShowDetails={item}
+            />
+          ))}
+        </ul>
+      </DiscoverySlider>
       {/*
         ------------------
         Streaming services 
@@ -48,19 +74,22 @@ const page = () => {
 
       {/* map through homeData and render a slider */}
       <Suspense
-        fallback={
-          <div className="master-container flex h-full w-full flex-col ">
+        fallback={Array.from({ length: 5 }).map((_, i) => (
+          <div
+            key={i}
+            className="master-container mt-4 flex h-full w-full flex-col"
+          >
             <h2 className="font-header-3 flex items-baseline font-bold capitalize text-white ">
-              Loading Content &nbsp;{" "}
+              Loading Category &nbsp;{" "}
               <AnimatedStringLoader loadingString="..." />
             </h2>
             <div className="mt-4 flex gap-x-2 overflow-hidden">
-              {Array.from({ length: 10 }, (_, i) => (
-                <DataFetchingMediaCardSkeleton key={i} />
+              {Array.from({ length: 8 }, (_, j) => (
+                <DataFetchingMediaCardSkeleton key={j} />
               ))}
             </div>
           </div>
-        }
+        ))}
       >
         <CategoriesSlider />
       </Suspense>
