5df93afd7f3e904b7fb6581cb68b6125b8663cd2
Ismail <shaikhnagismail@gmail.com>
2023-11-12 11:43:45 +0200

    Update avatar image paths

--- constants/avatars.ts
+++ constants/avatars.ts
@@ -1,27 +1,27 @@
 import { StaticImageData } from "next/image";
 
-import avatarOne from "@/assets/images/avatars/avatar-1.webp";
-import avatarTwo from "@/assets/images/avatars/avatar-2.webp";
-import avatarThree from "@/assets/images/avatars/avatar-3.webp";
-import avatarFour from "@/assets/images/avatars/avatar-4.webp";
-import avatarFive from "@/assets/images/avatars/avatar-5.webp";
-import avatarSix from "@/assets/images/avatars/avatar-6.webp";
-import avatarSeven from "@/assets/images/avatars/avatar-7.webp";
-import avatarEight from "@/assets/images/avatars/avatar-8.webp";
-import avatarNine from "@/assets/images/avatars/avatar-9.webp";
-import avatarTen from "@/assets/images/avatars/avatar-10.webp";
-import avatarEleven from "@/assets/images/avatars/avatar-11.webp";
-import avatarTwelve from "@/assets/images/avatars/avatar-12.webp";
-import avatarThirteen from "@/assets/images/avatars/avatar-13.webp";
-import avatarFourteen from "@/assets/images/avatars/avatar-14.webp";
-import avatarFifteen from "@/assets/images/avatars/avatar-15.webp";
-import avatarSixteen from "@/assets/images/avatars/avatar-16.webp";
-import avatarSeventeen from "@/assets/images/avatars/avatar-17.webp";
-import avatarEighteen from "@/assets/images/avatars/avatar-18.webp";
-import avatarNineteen from "@/assets/images/avatars/avatar-19.webp";
-import avatarTwenty from "@/assets/images/avatars/avatar-20.webp";
-import avatarTwentyOne from "@/assets/images/avatars/avatar-21.webp";
-import avatarTwentyTwo from "@/assets/images/avatars/avatar-22.webp";
+import avatarOne from "@/public/avatars/avatar-1.webp";
+import avatarTwo from "@/public/avatars/avatar-2.webp";
+import avatarThree from "@/public/avatars/avatar-3.webp";
+import avatarFour from "@/public/avatars/avatar-4.webp";
+import avatarFive from "@/public/avatars/avatar-5.webp";
+import avatarSix from "@/public/avatars/avatar-6.webp";
+import avatarSeven from "@/public/avatars/avatar-7.webp";
+import avatarEight from "@/public/avatars/avatar-8.webp";
+import avatarNine from "@/public/avatars/avatar-9.webp";
+import avatarTen from "@/public/avatars/avatar-10.webp";
+import avatarEleven from "@/public/avatars/avatar-11.webp";
+import avatarTwelve from "@/public/avatars/avatar-12.webp";
+import avatarThirteen from "@/public/avatars/avatar-13.webp";
+import avatarFourteen from "@/public/avatars/avatar-14.webp";
+import avatarFifteen from "@/public/avatars/avatar-15.webp";
+import avatarSixteen from "@/public/avatars/avatar-16.webp";
+import avatarSeventeen from "@/public/avatars/avatar-17.webp";
+import avatarEighteen from "@/public/avatars/avatar-18.webp";
+import avatarNineteen from "@/public/avatars/avatar-19.webp";
+import avatarTwenty from "@/public/avatars/avatar-20.webp";
+import avatarTwentyOne from "@/public/avatars/avatar-21.webp";
+import avatarTwentyTwo from "@/public/avatars/avatar-22.webp";
 
 export const avatars: StaticImageData[] = [
   avatarOne,
