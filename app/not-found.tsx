89c8e518a2d7ac6544b4ab9cbd01a5c57ffeff77
Ismail <shaikhnagismail@gmail.com>
2023-11-12 05:13:59 +0200

    Add image to 404 page and update button component.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,24 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
-import Navbar from "@/components/application-group/navbar/Navbar";
-import Footer from "@/components/Footer";
-import BottomMobileNavbar from "@/components/application-group/navbar/BottomMobileNavbar";
 
 import { BsArrowUpLeft } from "react-icons/bs";
-import { BsArrowLeft as LeftArrow } from "react-icons/bs";
+
+import { DetailsButton } from "@/components/DetailsButton";
+import Navbar from "@/components/application-group/navbar/Navbar";
+import BottomMobileNavbar from "@/components/application-group/navbar/BottomMobileNavbar";
+import Footer from "@/components/Footer";
+
+import notFoundMonkey from "@/assets/images/error/not-found-monkey.png";
 
 // bg-gradient-to-tr from-[#060212]  via-[rgba(6,2,18,8)] to-[#070739]
 
 export default function NotFound() {
-  const router = useRouter();
-
-  const handleGoBack = () => {
-    router.back();
-  };
-
   return (
     <section
+      role="main"
+      aria-label="404 Not Found"
       className="flex h-screen w-full flex-col items-center justify-center gap-y-10  text-white/50"
       style={{
         background:
@@ -30,37 +27,35 @@ export default function NotFound() {
     >
       <Navbar />
       <section className="flex flex-1 flex-col items-center justify-center text-center">
-        <h1
-          className="mb-2 bg-cover bg-clip-text bg-center text-[60px] font-bold leading-none text-transparent md:text-[100px] lg:text-[120px]"
-          style={{
-            backgroundImage: `url("/not-found-bg.jpg")`,
-            objectFit: "fill",
-          }}
-        >
-          We Lost You!
-        </h1>
-        <div className="text-center w-full ">
+        <Image
+          src={notFoundMonkey}
+          alt="Image of a monkey holding a magnifying glass indicating a 404 error"
+          className="object-contain"
+        />
+
+        <div className="w-full text-center ">
           <h2 className="mt-4 text-xl font-bold uppercase text-white/80 md:text-2xl lg:text-3xl">
             404 - Page not found
           </h2>
-          <p className="font-body-text mb-4 max-w-[90%] mx-auto font-normal lg:w-[38rem]">
+          <p className="font-body-text mb-4 font-normal ">
             Sorry, it seems you&apos;ve reached a dead end.
           </p>
         </div>
-        {/* <Link
-          href="/"
-          className="flex h-fit w-max items-center rounded-lg border-[#002be7ff] bg-[#0035F0FF] px-4 py-2 text-sm font-bold uppercase text-white outline outline-0 outline-[#002be7ff] hover:bg-[#002be7ff] hover:text-white hover:outline-2"
-        >
-          <BsArrowUpLeft className="mr-2 h-4 w-4" /> Return Home
-        </Link> */}
 
-        <Button
-          onClick={handleGoBack}
-          className="group flex h-fit w-max items-center rounded-lg border-[#002be7ff] bg-[#0035F0FF] px-4 py-2 text-sm font-bold uppercase text-white outline outline-0 outline-[#002be7ff] hover:bg-[#002be7ff] hover:text-white hover:outline-2"
+        <DetailsButton
+          asChild
+          variant={"tertiary"}
+          size={"sm"}
+          className="group gap-x-2 rounded-md text-sm font-bold uppercase"
         >
-          <LeftArrow className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-0.5 group-hover:scale-110" />{" "}
-          Go Back
-        </Button>
+          <Link href="/">
+            <BsArrowUpLeft
+              className="text-base transition-transform group-hover:scale-110"
+              aria-label="Return Home"
+            />{" "}
+            Return Home
+          </Link>
+        </DetailsButton>
       </section>
 
       <div
@@ -68,7 +63,7 @@ export default function NotFound() {
         absolute bottom-0 left-0 right-0
       "
       >
-        <Footer />        
+        <Footer />
       </div>
 
       <BottomMobileNavbar />
