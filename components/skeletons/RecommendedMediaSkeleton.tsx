094db7ee1e3be13a67a3d4da913a2a0903a9e560
Ismail <shaikhnagismail@gmail.com>
2023-11-13 23:14:30 +0200

    Update RecommendedMediaSkeleton to display more media cards and adjust their aspect ratio and grid layout

--- components/skeletons/RecommendedMediaSkeleton.tsx
+++ components/skeletons/RecommendedMediaSkeleton.tsx
@@ -1,14 +1,19 @@
 import CardSkeleton from "@/components/skeletons/CardSkeleton";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import DataFetchingMediaCardSkeleton from "./DataFetchingMediaCardSkeleton";
 
 const RecommendedMediaSkeleton = () => {
   return (
-    <div className="grid  grid-cols-2  gap-x-4 gap-y-12 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid  grid-cols-2  gap-x-4 gap-y-14 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6">
       {Array.from({ length: 8 }).map((_, index) => (
         <div key={index} className="relative w-full ">
-          <AspectRatio ratio={16 / 9}>
+          <AspectRatio ratio={2 / 3}>
             <CardSkeleton rows={0} />
           </AspectRatio>
+
+          <p className="text-sm md:text-base">
+            <CardSkeleton rows={1} />
+          </p>
         </div>
       ))}
     </div>
