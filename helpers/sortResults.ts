2fe50e1a449e960525e86c310e0a9323dba1c56b
Ismail <shaikhnagismail@gmail.com>
2023-11-19 11:52:24 +0200

    Refactor sortResultsByPopularity and add support for sorting by vote count

--- helpers/sortResults.ts
+++ helpers/sortResults.ts
@@ -1,7 +1,16 @@
-export const sortResultsByPopularity = <T extends { popularity: number }>(
-  results: T[],
-) => results.sort((a, b) => b.popularity - a.popularity);
+// Define a generic type that extends an object with a popularity property
+type ResultWithPopularity = { popularity: number };
 
-export const sortResultsByVoteCount = <T extends { vote_count: number }>(
+// Modify the sortResultsByPopularity function to accept an array of objects with a popularity property
+export function sortResultsByPopularity<T extends ResultWithPopularity>(
+  results: T[]
+): T[] {
+  return results.sort((a, b) => b.popularity - a.popularity);
+}
+
+// ===============================================
+type ResultWithVoteCount = { vote_count: number };
+
+export const sortResultsByVoteCount = <T extends ResultWithVoteCount>(
   results: T[],
 ) => results.sort((a, b) => b.vote_count - a.vote_count);
